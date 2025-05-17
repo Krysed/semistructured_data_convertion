5c8704d62ba2625ae77def6b093520306ffc86d0
Krystian Fryca <kp15731@gmail.com>
2025-05-17 22:56:03 +0200

    [feat-display_data] - displaying generated data in html

--- xml_gen/generate_xml.py
+++ xml_gen/generate_xml.py
@@ -8,16 +8,28 @@ fake = Faker('pl_PL')
 def generate_user_data(num_users: int, currency: str) -> dict:
     users = []
     for _ in range(num_users):
+        first = fake.first_name()
+        last = fake.last_name()
+        username = random.choice([fake.user_name(),f"{first.lower()}.{last.lower()}"])
+        city = fake.city()
+        email_domain = random.choice(["example.com", "mail.com", "test.org"])
+        num = random.randint(10, 9999)
+        email = random.choice([
+            f"{first.lower()}.{last.lower()}{num}@{email_domain}",
+            f"{username}{num}@{email_domain}",
+            f"{first.lower()}{num}@{email_domain}",
+            f"{first.lower()}_{last.lower()}@{email_domain}"
+        ])
         users.append({
-            "name": fake.first_name(),
-            "last_name": fake.last_name(),
-            "username": fake.user_name(),
-            "date_of_birth": fake.date_of_birth(),
-            "email": fake.email(),
+            "first_name": last,
+            "last_name": first,
+            "username": username,
+            "date_of_birth": str(fake.date_of_birth()),
+            "email": email,
             "password": fake.password(),
             "role": random.choice(["user", "guest"]),
             "debt": round(random.uniform(1000, 100000), 2),
-            "city": fake.city(),
+            "city": city,
             "currency": currency,
         })
     return {"root": {"user": users}}
